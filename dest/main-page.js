41cec693984fb0d7f84210e0b8f860ad27605bb7
Trung Phạm <TrungPham@users.noreply.github.com>
2022-03-30 17:26:43 +0700

    push cho thằng em học hỏi

--- dest/main-page.js
+++ dest/main-page.js
@@ -5,6 +5,7 @@ $('.carousel').flickity({
     draggable: true,
     selectedAttraction: 0.01,
     friction: 0.15,
+    imagesLoaded: true,
     lazyLoad: true,
     on: {
         ready: function (index) {
@@ -18,14 +19,31 @@ $('.carousel').flickity({
         }
     }
 });
+var $carousel = $('.carousel').flickity();
+var flkty = $carousel.data('flickity');
+var $imgs = $('.carousel-cell .image img');
+console.log($imgs);
+$carousel.on('scroll.flickity', function (event, progress) {
+    flkty.slides.forEach(function (slide, i) {
+        var img = $imgs[i];
+        var x = (slide.target + flkty.x) * -1 / 3;
+        img.style.transform = 'translateX( ' + x + 'px)';
+    });
+});
 
-// let currentIndex = 0;
-// let sliderChild = $('.slider_child');
-// let buttons = $('.flickity-prev-next-button');
-// console.log(sliderChild);
-// buttons.on('click', function () {
-//     let sliderSelected = $('.slider_child.is-selected');
-//     $('.slider_child .name').removeClass('active');
-// });
-// sliderChild.childNodes[3].removeClass('active');
-// sliderSelected.childNodes[3].addClass('active');
+//scroll header
+window.addEventListener('scroll', function () {
+    var heightScroll = document.documentElement.scrollTop;
+    console.log(heightScroll);
+    if (heightScroll > 150) {
+        $('.header').addClass('active');
+        // transform: translateY(0px);
+        // $('.header').translateY = 'transform: translateY(0px)';
+        // this.setTimeout(function () {
+        //     document.querySelector('.header').classList.add('active-v2');
+        // }, 700)
+    } else {
+        $('.header').removeClass('active')
+        // document.querySelector('.header').classList.remove('active-v2');
+    }
+})
